ebdc5168ca339f6b2a7c3dee0ebce5ff12cbb784
averagge <aleksejporfirev15@gmail.com>
2023-03-19 19:52:39 +0300

    Добавил классы Init, Operand, Operator, OperatorContainer, OperatorMethod

---
+++ lr9/lr9/Init.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LR8lib
+{
+    static public class Init
+    {
+        public static Bitmap bitmap;
+        public static PictureBox pictureBox;
+        public static Pen pen;
+        public static ListBox listBox;
+    }
+}
+
