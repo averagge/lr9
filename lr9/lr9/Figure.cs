5b9e62ba0927f8fc69a18ed3dbcb48c36c3670ef
averagge <aleksejporfirev15@gmail.com>
2023-03-19 19:12:39 +0300

    Добавил абстрактный класс фигур и наследуемые классы прямоугольника и квадрата

---
+++ lr9/lr9/Figure.cs
@@ -0,0 +1,55 @@
+﻿using LR8lib;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LR8lib
+{
+    abstract public class Figure
+    {
+
+        public int x;
+        public int y;
+        public int w;
+        public int h;
+        abstract public void Draw();
+        abstract public void MoveTo(int x, int y);
+        public void Clear()
+        {
+            Graphics g = Graphics.FromImage(Init.bitmap);
+            g.Clear(Color.White);
+        }
+        public void DeleteF(Figure figure, bool flag = true)
+        {
+            if (flag == true)
+            {
+                Graphics g = Graphics.FromImage(Init.bitmap);
+                ShapeContainer.figureList.Remove(figure);
+                this.Clear();
+                Init.pictureBox.Image = Init.bitmap;
+                foreach (Figure f in ShapeContainer.figureList)
+                {
+                    f.Draw();
+                }
+            }
+            else
+            {
+                Graphics g = Graphics.FromImage(Init.bitmap);
+                ShapeContainer.figureList.Remove(figure);
+                this.Clear();
+                Init.pictureBox.Image = Init.bitmap; //!
+                foreach (Figure f in ShapeContainer.figureList)
+                {
+                    f.Draw();
+                }
+                ShapeContainer.figureList.Add(figure);
+            }
+        }
+
+    }
+}
+
---
+++ lr9/lr9/Squre.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR8lib
+{
+
+        public class Square : Rectangle
+        {
+            public Square(int x, int y, int w) : base(x, y, w, w)
+            { 
+            }
+        }
+
+}
